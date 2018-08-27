069cf18f406c722ec4220986c863decc0322f044
niketajn <niktajn1@gmail.com>
2018-08-27 22:40:48 +0530

    refactor code-generated random number for each row and added delete button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+	
+
 	 $(document).ready(function(){
+	 	 
 		 	$('#users-data #users-table').hide();
 		 	$('#form-data-users #myForm').hide();
 
@@ -18,6 +21,13 @@
 
 			 $.each(usersList,function(index,value){
 			 	var tr = $("<tr></tr>");
+
+			 	var tdzero = $("<td></td>");
+			 	tdzero.addClass('userId');
+			 	tdzero.css('display','none');
+			 	tdzero.html(value._id);
+			 	tr.append(tdzero)
+
 			 	var td1 = $("<td></td>");
 			 	td1.html(value.name);
 			 	tr.append(td1);
@@ -37,22 +47,51 @@
 			 	td4.html(value.phoneNumber);
 			 	tr.append(td4);
 
+			 	var td5 = $("<td></td>");
+			 	td5.html('<button class="btn btn-danger btn-xs deleteBtn">DELETE</button>');
+			 	
+			 	td5.click(deleteUser) //tell jquery to call deleteU function when user performs 'click'
+			 	tr.append(td5);
+
 			 	$("#table-content").append(tr);
 
 			 	$('#users-data #users-table').show();
 			 	$('#form-data-users #myForm').hide();
+
+
 			 });
 		}
     var usersList = [];
     $('#myForm').submit(function(e){
         e.preventDefault();
-           var obj = {
+        var obj = {
+          _id: guidGenerator(),
           name: $("input[name=user-name]").val(),
           address: $("input[name=address]").val(),
           phoneNumber: $("input[name=phnum]").val(),
           email: $("input[name=email]").val(),
-          }
+        }
         usersList.push(obj);
         refreshList(usersList);
     });
+
+    function deleteUser(event){
+    	var button = $(event.target);  //which button is clicked
+    	var tr = button.parent().parent()
+
+
+    	var randomID = tr.find('.userId').html(); //find inside tr children
+    	console.log(randomID);
+    }
+
+    
+
+    function guidGenerator() {
+	    var S4 = function() {
+	       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+	    };
+	    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+	}
+
+//document ready    
 });
